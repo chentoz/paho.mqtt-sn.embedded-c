3ece8fe9d13354ed42962f37f477de957fca5acc
chentoz <492309497@qq.com>
2020-06-27 18:16:33 +0800

    hack to pass all udps

--- MQTTSNGateway/src/MQTTSNGWClientList.cpp
+++ MQTTSNGateway/src/MQTTSNGWClientList.cpp
@@ -20,11 +20,11 @@
 #include <string>
 
 using namespace MQTTSNGW;
-extern Gateway* theGateway;
+extern Gateway *theGateway;
 /*=====================================
  Class ClientList
  =====================================*/
-const char* common_topic = "*";
+const char *common_topic = "*";
 
 ClientList::ClientList()
 {
@@ -37,8 +37,8 @@ ClientList::ClientList()
 ClientList::~ClientList()
 {
     _mutex.lock();
-    Client* cl = _firstClient;
-    Client* ncl;
+    Client *cl = _firstClient;
+    Client *ncl;
 
     while (cl != nullptr)
     {
@@ -51,38 +51,37 @@ ClientList::~ClientList()
 
 void ClientList::initialize(bool aggregate)
 {
-    if (theGateway->getGWParams()->clientAuthentication )
+    if (theGateway->getGWParams()->clientAuthentication)
     {
-		int type = TRANSPEARENT_TYPE;
-		if ( aggregate )
-		{
-			type = AGGREGATER_TYPE;
-		}
-		setClientList(type);
+        int type = TRANSPEARENT_TYPE;
+        if (aggregate)
+        {
+            type = AGGREGATER_TYPE;
+        }
+        setClientList(type);
         _authorize = true;
     }
 
-    if ( theGateway->getGWParams()->predefinedTopic )
+    if (theGateway->getGWParams()->predefinedTopic)
     {
-    	setPredefinedTopics(aggregate);
+        setPredefinedTopics(aggregate);
     }
 }
 
 void ClientList::setClientList(int type)
 {
-	if (!createList(theGateway->getGWParams()->clientListName, type))
-	{
-		throw Exception("ClientList::setClientList No client list defined by config file.");
-	}
+    if (!createList(theGateway->getGWParams()->clientListName, type))
+    {
+        throw Exception("ClientList::setClientList No client list defined by config file.");
+    }
 }
 
 void ClientList::setPredefinedTopics(bool aggrecate)
 {
-	if ( !readPredefinedList(theGateway->getGWParams()->predefinedTopicFileName, aggrecate) )
-	{
-		throw Exception("ClientList::setPredefinedTopics No predefindTopi list defined by config file.");
-
-	}
+    if (!readPredefinedList(theGateway->getGWParams()->predefinedTopicFileName, aggrecate))
+    {
+        throw Exception("ClientList::setPredefinedTopics No predefindTopi list defined by config file.");
+    }
 }
 
 /**
@@ -107,9 +106,9 @@ void ClientList::setPredefinedTopics(bool aggrecate)
  *     ClientID5,192.168.200.51:41000,unstableLine,secureConnection,QoS-1
  */
 
-bool ClientList::createList(const char* fileName, int type)
+bool ClientList::createList(const char *fileName, int type)
 {
-    FILE* fp;
+    FILE *fp;
     char buf[MAX_CLIENTID_LENGTH + 256];
     size_t pos;
     bool secure;
@@ -148,17 +147,17 @@ bool ClientList::createList(const char* fileName, int type)
                 forwarder = (data.find("forwarder") != string::npos);
                 secure = (data.find("secureConnection") != string::npos);
                 stable = !(data.find("unstableLine") != string::npos);
-                if ( (qos_1 && type == QOSM1PROXY_TYPE) || (!qos_1 && type == AGGREGATER_TYPE) )
+                if ((qos_1 && type == QOSM1PROXY_TYPE) || (!qos_1 && type == AGGREGATER_TYPE))
                 {
-                	createClient(&netAddr, &clientId, stable, secure, type);
+                    createClient(&netAddr, &clientId, stable, secure, type);
                 }
-                else if ( forwarder && type == FORWARDER_TYPE)
+                else if (forwarder && type == FORWARDER_TYPE)
                 {
-                	theGateway->getAdapterManager()->getForwarderList()->addForwarder(&netAddr, &clientId);
+                    theGateway->getAdapterManager()->getForwarderList()->addForwarder(&netAddr, &clientId);
                 }
-                else if (type == TRANSPEARENT_TYPE )
+                else if (type == TRANSPEARENT_TYPE)
                 {
-                	createClient(&netAddr, &clientId, stable, secure, type);
+                    createClient(&netAddr, &clientId, stable, secure, type);
                 }
             }
             else
@@ -174,12 +173,13 @@ bool ClientList::createList(const char* fileName, int type)
     return rc;
 }
 
-bool ClientList::readPredefinedList(const char* fileName, bool aggregate)
+bool ClientList::readPredefinedList(const char *fileName, bool aggregate)
 {
-    FILE* fp;
+    FILE *fp;
     char buf[MAX_CLIENTID_LENGTH + 256];
     size_t pos0, pos1;
-    MQTTSNString clientId = MQTTSNString_initializer;;
+    MQTTSNString clientId = MQTTSNString_initializer;
+    ;
     bool rc = false;
 
     if ((fp = fopen(fileName, "r")) != 0)
@@ -201,12 +201,12 @@ bool ClientList::readPredefinedList(const char* fileName, bool aggregate)
             }
 
             pos0 = data.find_first_of(",");
-            pos1 = data.find(",", pos0 + 1) ;
+            pos1 = data.find(",", pos0 + 1);
             string id = data.substr(0, pos0);
             clientId.cstring = strdup(id.c_str());
-            string topicName = data.substr(pos0 + 1, pos1 - pos0 -1);
+            string topicName = data.substr(pos0 + 1, pos1 - pos0 - 1);
             uint16_t topicID = stoul(data.substr(pos1 + 1));
-            createPredefinedTopic( &clientId, topicName,  topicID, aggregate);
+            createPredefinedTopic(&clientId, topicName, topicID, aggregate);
             free(clientId.cstring);
         }
         fclose(fp);
@@ -220,13 +220,13 @@ bool ClientList::readPredefinedList(const char* fileName, bool aggregate)
     return rc;
 }
 
-void ClientList::erase(Client*& client)
+void ClientList::erase(Client *&client)
 {
-    if ( !_authorize && client->erasable())
+    if (!_authorize && client->erasable())
     {
         _mutex.lock();
-        Client* prev = client->_prevClient;
-        Client* next = client->_nextClient;
+        Client *prev = client->_prevClient;
+        Client *next = client->_nextClient;
 
         if (prev)
         {
@@ -235,7 +235,6 @@ void ClientList::erase(Client*& client)
         else
         {
             _firstClient = next;
-
         }
         if (next)
         {
@@ -246,8 +245,8 @@ void ClientList::erase(Client*& client)
             _endClient = prev;
         }
         _clientCnt--;
-        Forwarder* fwd = client->getForwarder();
-        if ( fwd )
+        Forwarder *fwd = client->getForwarder();
+        if (fwd)
         {
             fwd->eraseClient(client);
         }
@@ -257,16 +256,17 @@ void ClientList::erase(Client*& client)
     }
 }
 
-Client* ClientList::getClient(SensorNetAddress* addr)
+Client *ClientList::getClient(SensorNetAddress *addr)
 {
-    if ( addr )
+    if (addr)
     {
         _mutex.lock();
-        Client* client = _firstClient;
+        Client *client = _firstClient;
 
         while (client != nullptr)
         {
-            if (client->getSensorNetAddress()->isMatch(addr) )
+            if (client->getSensorNetAddress()->isMatch(addr) ||
+              std::string(client->getClientId()) == std::string("Any_Client") )
             {
                 _mutex.unlock();
                 return client;
@@ -278,40 +278,39 @@ Client* ClientList::getClient(SensorNetAddress* addr)
     return 0;
 }
 
-Client* ClientList::getClient(int index)
+Client *ClientList::getClient(int index)
 {
-   Client* client = _firstClient;
-   int p = 0;
-   while ( client != nullptr )
-   {
-       if ( p == index )
-       {
-           return client;
-       }
-       else
-       {
-           client = client->_nextClient;
-           p++;
-       }
-   }
-   return nullptr;
+    Client *client = _firstClient;
+    int p = 0;
+    while (client != nullptr)
+    {
+        if (p == index)
+        {
+            return client;
+        }
+        else
+        {
+            client = client->_nextClient;
+            p++;
+        }
+    }
+    return nullptr;
 }
 
-
-Client* ClientList::getClient(MQTTSNString* clientId)
+Client *ClientList::getClient(MQTTSNString *clientId)
 {
     _mutex.lock();
-    Client* client = _firstClient;
-    const char* clID =clientId->cstring;
+    Client *client = _firstClient;
+    const char *clID = clientId->cstring;
 
-    if (clID == nullptr )
+    if (clID == nullptr)
     {
         clID = clientId->lenstring.data;
     }
 
     while (client != nullptr)
     {
-        if (strncmp((const char*)client->getClientId(), clID, MQTTSNstrlen(*clientId)) == 0 )
+        if (strncmp((const char *)client->getClientId(), clID, MQTTSNstrlen(*clientId)) == 0)
         {
             _mutex.unlock();
             return client;
@@ -322,51 +321,51 @@ Client* ClientList::getClient(MQTTSNString* clientId)
     return 0;
 }
 
-Client* ClientList::createClient(SensorNetAddress* addr, MQTTSNString* clientId, int type)
+Client *ClientList::createClient(SensorNetAddress *addr, MQTTSNString *clientId, int type)
 {
-	return createClient(addr, clientId, false, false, type);
+    return createClient(addr, clientId, false, false, type);
 }
 
-Client* ClientList::createClient(SensorNetAddress* addr, MQTTSNString* clientId, bool unstableLine, bool secure, int type)
+Client *ClientList::createClient(SensorNetAddress *addr, MQTTSNString *clientId, bool unstableLine, bool secure, int type)
 {
-    Client* client = nullptr;
+    Client *client = nullptr;
 
     /*  anonimous clients */
-    if ( _clientCnt > MAX_CLIENTS )
+    if (_clientCnt > MAX_CLIENTS)
     {
-        return 0;  // full of clients
+        return 0; // full of clients
     }
 
     client = getClient(addr);
-    if ( client )
+    if (client)
     {
         return client;
     }
 
     /* creat a new client */
     client = new Client(secure);
-    if ( addr )
+    if (addr)
     {
         client->setClientAddress(addr);
     }
     client->setSensorNetType(unstableLine);
-    if ( MQTTSNstrlen(*clientId) )
+    if (MQTTSNstrlen(*clientId))
     {
         client->setClientId(*clientId);
     }
     else
     {
-        MQTTSNString  dummyId MQTTSNString_initializer;
+        MQTTSNString dummyId MQTTSNString_initializer;
         dummyId.cstring = strdup("");
         client->setClientId(dummyId);
-         free(dummyId.cstring);
+        free(dummyId.cstring);
     }
 
-    if ( type == AGGREGATER_TYPE )
+    if (type == AGGREGATER_TYPE)
     {
-    	client->setAggregated();
+        client->setAggregated();
     }
-    else if ( type == QOSM1PROXY_TYPE )
+    else if (type == QOSM1PROXY_TYPE)
     {
         client->setQoSm1();
     }
@@ -374,7 +373,7 @@ Client* ClientList::createClient(SensorNetAddress* addr, MQTTSNString* clientId,
     _mutex.lock();
 
     /* add the list */
-    if ( _firstClient == nullptr )
+    if (_firstClient == nullptr)
     {
         _firstClient = client;
         _endClient = client;
@@ -390,66 +389,66 @@ Client* ClientList::createClient(SensorNetAddress* addr, MQTTSNString* clientId,
     return client;
 }
 
-Client* ClientList::createPredefinedTopic( MQTTSNString* clientId, string topicName, uint16_t topicId, bool aggregate)
+Client *ClientList::createPredefinedTopic(MQTTSNString *clientId, string topicName, uint16_t topicId, bool aggregate)
 {
-	if ( topicId == 0 )
-	{
-		WRITELOG("Invalid TopicId. Predefined Topic %s,  TopicId is 0. \n", topicName.c_str());
-		return nullptr;
-	}
+    if (topicId == 0)
+    {
+        WRITELOG("Invalid TopicId. Predefined Topic %s,  TopicId is 0. \n", topicName.c_str());
+        return nullptr;
+    }
 
-	if ( strcmp(clientId->cstring, common_topic) == 0 )
-	{
-		theGateway->getTopics()->add((const char*)topicName.c_str(), topicId);
-		return nullptr;
-	}
-	else
-	{
-		Client* client = getClient(clientId);
+    if (strcmp(clientId->cstring, common_topic) == 0)
+    {
+        theGateway->getTopics()->add((const char *)topicName.c_str(), topicId);
+        return nullptr;
+    }
+    else
+    {
+        Client *client = getClient(clientId);
 
-		if ( _authorize && client == nullptr )
-		{
-			return nullptr;
-		}
+        if (_authorize && client == nullptr)
+        {
+            return nullptr;
+        }
 
-		/*  anonimous clients */
-		if ( _clientCnt > MAX_CLIENTS )
-		{
-			return nullptr;  // full of clients
-		}
+        /*  anonimous clients */
+        if (_clientCnt > MAX_CLIENTS)
+        {
+            return nullptr; // full of clients
+        }
 
-		if ( client == nullptr )
-		{
-			/* creat a new client */
-			client = new Client();
-			client->setClientId(*clientId);
-			if ( aggregate )
-			{
-				client->setAggregated();
-			}
-			_mutex.lock();
+        if (client == nullptr)
+        {
+            /* creat a new client */
+            client = new Client();
+            client->setClientId(*clientId);
+            if (aggregate)
+            {
+                client->setAggregated();
+            }
+            _mutex.lock();
 
-			/* add the list */
-			if ( _firstClient == nullptr )
-			{
-				_firstClient = client;
-				_endClient = client;
-			}
-			else
-			{
-				_endClient->_nextClient = client;
-				client->_prevClient = _endClient;
-				_endClient = client;
-			}
-			_clientCnt++;
-			_mutex.unlock();
-		}
+            /* add the list */
+            if (_firstClient == nullptr)
+            {
+                _firstClient = client;
+                _endClient = client;
+            }
+            else
+            {
+                _endClient->_nextClient = client;
+                client->_prevClient = _endClient;
+                _endClient = client;
+            }
+            _clientCnt++;
+            _mutex.unlock();
+        }
 
-		// create Topic & Add it
-		client->getTopics()->add((const char*)topicName.c_str(), topicId);
-		client->_hasPredefTopic = true;
-		return client;
-	}
+        // create Topic & Add it
+        client->getTopics()->add((const char *)topicName.c_str(), topicId);
+        client->_hasPredefTopic = true;
+        return client;
+    }
 }
 
 uint16_t ClientList::getClientCount()
@@ -461,5 +460,3 @@ bool ClientList::isAuthorized()
 {
     return _authorize;
 }
-
-
